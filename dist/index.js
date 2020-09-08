b2a8f86b6aa1a242bfa0044fc3a063488914f566
hackerbuddy229 <rasmusb229@gmail.com>
2020-09-08 10:27:19 +0200

    post labb

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var port = 3000;
-app.get('/', function (req, res) { return res.sendFile(__dirname + '/index.html'); });
+app.use(express.json());
 app.use(express.static(__dirname + "/wwwroot/"));
+app.get('/', function (req, res) { return res.sendFile(__dirname + '/index.html'); });
+app.post('/post', function (req, res) {
+    var name = req.body["name"] || "NO NAME";
+    res.send("Hi " + name + "!");
+});
 app.listen(port, function () { return console.log("Example app listening on port port!"); });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.static(__dirname + "/wwwroot/"));
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
+app.post('/post', (req, res) => {
+    let name : string = req.body["name"] || "NO NAME";
+    
+    res.send(`Hi ${name}!`);
 
-app.use(express.static(__dirname + "/wwwroot/"));
+    //res.json(req.body);
+});
+
+
+
 app.listen(port, () => console.log(`Example app listening on port port!`));
